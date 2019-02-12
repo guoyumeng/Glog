0dc97f423f7399b7568b69b2d3530454d269df66
guoyumeng <guoyumengcn@gmail.com>
2019-02-12 12:12:43 +0800

    no message

--- src/components/login/register.vue
+++ src/components/login/register.vue
@@ -15,6 +15,15 @@
                         <el-radio v-model="sex" label="女">女</el-radio>
                         <el-radio v-model="sex" label="保密">保密</el-radio>
                     </span>
+                    <el-upload
+                    class="avatar-uploader"
+                    action="https://jsonplaceholder.typicode.com/posts/"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload">
+                        <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                    </el-upload>
                     <div>
                         <el-button type="primary" size="medium" round @click="reg()">注册</el-button>
                     </div>
@@ -38,9 +47,26 @@
             password: '',
             rep_pass: '',
             sex: '',
+            imageUrl: '',
       };
     },
     methods: {
+
+        handleAvatarSuccess(res, file) {
+            this.imageUrl = URL.createObjectURL(file.raw);
+        },
+        beforeAvatarUpload(file) {
+            const isJPG = file.type === 'image/jpeg';
+            const isLt2M = file.size / 1024 / 1024 < 2;
+
+            if (!isJPG) {
+            this.$message.error('上传头像图片只能是 JPG 格式!');
+            }
+            if (!isLt2M) {
+            this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            return isJPG && isLt2M;
+        },
 
         setcookie(key,value,days) {
             var d = new Date();
@@ -187,4 +213,30 @@
 
 
     
+</style>
+
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
